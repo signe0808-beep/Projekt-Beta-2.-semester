4a625e902a0ada443e03054751b631618a7e0686
signe <signe0808@gmail.com>
2026-04-16 22:41:12 +0200

    hej

---
+++ TESTAvaloniaApplication/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TESTAvaloniaApplication.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ TESTAvaloniaApplication/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TESTAvaloniaApplication.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
