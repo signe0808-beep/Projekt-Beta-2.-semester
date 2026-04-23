3c3c44746f9890fd73e7215537eb8ce45d896bc2
linneamaren <au793664@uni.au.dk>
2026-04-23 10:34:59 +0200

    h

---
+++ DataAccess/Drivers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTAvaloniaApplication.DataLayer.Drivers
+{
+    internal class Class1
+    {
+    }
+}
