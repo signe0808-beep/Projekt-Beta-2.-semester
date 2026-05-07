a2bd2771d1581ea02a99ad954e5123a9dfe3885b
marcu <marcu@LAPTOP-4L9VSAR0>
2026-05-07 12:49:41 +0200

    rettede flere ting

--- TESTAvaloniaApplication/ViewModels/MainWindowViewModel.cs
+++ TESTAvaloniaApplication/ViewModels/MainWindowViewModel.cs
@@ -1,26 +1,39 @@
-﻿using BusinessLayer.Services;
+using BusinessLayer.Services;
 using TESTAvaloniaApplication.DataAccess.Simulators;
+using TESTAvaloniaApplication.BusinessLayer.Models;
+using Avalonia.Threading;
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace TESTAvaloniaApplication.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        // Dette er constructoren. Den kører, når skærmen åbner.
-        // 1. Opret variablen HERUDE, så den overlever!
         private PressureMonitor _minMotor;
+
+        [ObservableProperty]
+        private string _statusText = "Status: Normal";
 
         public MainWindowViewModel()
         {
             var minFalskeSensor = new TestSimulator();
-
-            // 2. Gem motoren i vores nye, sikre variabel (fjern ordet 'var')
             _minMotor = new PressureMonitor(minFalskeSensor);
 
-            // 3. Start motoren
+            // Opdaterer StatusText hvert 100ms baseret på systemets tilstand
+            DispatcherTimer.Run(() =>
+            {
+                StatusText = _minMotor.CurrentState == SystemStateEnum.Alarm
+                    ? "Status: ALARM!"
+                    : "Status: Normal";
+                return true;
+            }, TimeSpan.FromMilliseconds(100));
+        }
+
+        [RelayCommand]
+        private void StartCalibration()
+        {
             _minMotor.StartSystem();
         }
     }
-
-   
-    
 }
