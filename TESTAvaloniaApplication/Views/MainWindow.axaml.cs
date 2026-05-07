6b9f1fdab06ed8943a504dbfa0c500825a09cfef
marcu <marcu@LAPTOP-4L9VSAR0>
2026-05-07 14:04:02 +0200

    ændret i hvordan UI opfører sig

--- TESTAvaloniaApplication/Views/MainWindow.axaml.cs
+++ TESTAvaloniaApplication/Views/MainWindow.axaml.cs
@@ -1,30 +1,14 @@
 using Avalonia.Controls;
-using Presentation.ViewModels;
-using BusinessLayer.Services;
-using TESTAvaloniaApplication.DataAccess.Simulators;
+using TESTAvaloniaApplication.ViewModels;
 
 namespace Presentation
 {
     public partial class MainWindow : Window
     {
-        private PressureMonitor _logic;
         public MainWindow()
         {
             InitializeComponent();
-
-            var sensor = new TestSimulator();
-            _logic = new PressureMonitor(sensor);
-
-
-
-            Heatmap.DataContext = new HeatmapViewModel(_logic);
-
-            KalibrerButton.Click += KalibrerButton_Click;
-        }
-
-        private void KalibrerButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)  // Starter state machine og målinger når man trykker på kalibrer system knappen
-        {
-            _logic.StartSystem();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
