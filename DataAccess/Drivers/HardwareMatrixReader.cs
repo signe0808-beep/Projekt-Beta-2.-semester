20e4f6a0c9ce7a59ca8e1866e769e01bc579860d
signe <signe0808@gmail.com>
2026-05-20 14:45:48 +0200

    hjkh

--- DataAccess/Drivers/HardwareMatrixReader.cs
+++ DataAccess/Drivers/HardwareMatrixReader.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using DataAccess.Interfaces;
+using Iot.Device.Adc; // Her bruger vi Iot.Device.Bindings
+using System;
 using System.Device.Gpio;
 using System.Device.Spi;
-using Iot.Device.Adc; // Her bruger vi Iot.Device.Bindings
 using DataAccess.Interfaces;
 
 //Jeg har downloadet de pakker vi skal bruge for at kunne snakke sammen med RPi og AD converteren
-
 //Logikken er det som HW snakkede om, med at tænde strøm for 1 række ad gangen og læse hver kolonne.
 namespace TESTAvaloniaApplication.DataAccess.Drivers
 {
     // Implementerer ISensorReader til datalaget
-    //IDisposable er et interfcae, der sikrer, at systemet rydder op
+    //IDisposable er et interface, der sikrer, at systemet rydder op
     //og lukker de fysiske hardware-forbindelser sikkert ned,
     //når programmet er færdig med at bruge dem,
     //så benene på Raspberry Pi'en ikke bliver efterladt tændte eller låste.
@@ -30,8 +30,8 @@ namespace TESTAvaloniaApplication.DataAccess.Drivers
             _gpio = new GpioController();
             foreach (var pin in _rowPins)
             {
-                _gpio.OpenPin(pin, PinMode.Output);
-                _gpio.Write(pin, PinValue.Low); //Sætter pin til Low (slukket) som udgangspunkt
+                // ÆNDRING 1: Start altid rækker som Input (frakoblet/High-Z) fremfor Output/Low
+                _gpio.OpenPin(pin, PinMode.Input);
             }
 
             // Gør SPI-forbindelsen klar til MCP3008
@@ -39,7 +39,7 @@ namespace TESTAvaloniaApplication.DataAccess.Drivers
             var spiConnectionSettings = new SpiConnectionSettings(0, 0)
             {
                 ClockFrequency = 1000000, // 1 MHz skal evt. ændres
-                Mode = SpiMode.Mode0 //dette er inbygget enum. 0 betyder at vi indstiller hvornår de skal læse clocksignalet
+                Mode = SpiMode.Mode0 //dette er indbygget enum. 0 betyder at vi indstiller hvornår de skal læse clocksignalet
             };
 
             _spiDevice = SpiDevice.Create(spiConnectionSettings);
@@ -55,21 +55,22 @@ namespace TESTAvaloniaApplication.DataAccess.Drivers
             // Kør alle 4 rækker igennem en ad gangen
             for (int r = 0; r < 4; r++)
             {
-                // Sætter den aktuelle række til High (sender strøm igennem Velostat-materialet)
+                // ÆNDRING 2: Gør den aktuelle række til Output, før den sættes High
+                _gpio.SetPinMode(_rowPins[r], PinMode.Output);
                 _gpio.Write(_rowPins[r], PinValue.High);
 
                 // Kort delay (1 ms) sikrer, at spændingen stabiliserer sig før aflæsning
+                // NOTE: Hvis Velostaten er sløv til at reagere, kan I prøve at sætte denne til 5 eller 10 ms.
                 System.Threading.Thread.Sleep(1);
 
                 // Aflæser de 4 kolonner via AD-konverteren
-                // Det forudsættes, at kolonnerne er tilsluttet kanal 0, 1, 2 og 3 på MCP3008
                 matrix[r, 0] = _mcp.Read(0);
                 matrix[r, 1] = _mcp.Read(1);
                 matrix[r, 2] = _mcp.Read(2);
                 matrix[r, 3] = _mcp.Read(3);
 
-                // Sætter rækken til Low (slukker strømmen), inden næste iteration
-                _gpio.Write(_rowPins[r], PinValue.Low);
+                // ÆNDRING 3: I stedet for at sætte rækken til Low, sætter vi den tilbage til Input (frakoblet)
+                _gpio.SetPinMode(_rowPins[r], PinMode.Input);
             }
 
             return matrix;
